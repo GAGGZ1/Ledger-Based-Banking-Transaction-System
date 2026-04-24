bb958670df3fa18f52d27f5de8b2992a8dc194a7
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-04-24 23:12:55 +0530

    fix: add cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cookieParser = require("cookie-parser")
 const swaggerUi = require("swagger-ui-express")
 const swaggerSpec = require("./config/swagger")
+const cors = require("cors")
 
 const app = express()
 
